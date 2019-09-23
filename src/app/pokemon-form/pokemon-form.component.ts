15fb5530e4acade4f9fdf8a5c76bf50221002082
Ernany Gonçalves <goncalves.ernany@gmail.com>
2019-09-23 17:44:20 -0300

    Ajustes no formulario prévia. Talves seja necessario alterar o modelo do backend para q a lista de evolucoes apareça na previa ao editar. ATENÇAO MAIOR AOS TIPOS E MELHORAR COMO AS EVOLUÇÕES ESTÃO SENDO ADICIONADAS AO ARRAY. Criar ngif pra ir mostrando certas partes da prévia ao cadastrar.

--- src/app/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon-form/pokemon-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpPokemonBdService } from '../http-pokemon-bd.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-form',
@@ -11,22 +12,47 @@ export class PokemonFormComponent implements OnInit {
 
   types: string[] = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"];
 
+  // evolutions = [{ number: 25, name: 'Ernany' },{ number: 26, name: 'Daniel' }];
+  evolutions: { 'number': number, 'string': string }[] = [];
+
   isOnEditMode: boolean = false;
 
   pokemon = {};
 
-  constructor(private reqPokemon: HttpPokemonBdService ,private route: ActivatedRoute) { }
+  constructor(private reqPokemon: HttpPokemonBdService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    if (this.route.snapshot.queryParams.formMode === "edit") { 
+    if (this.route.snapshot.queryParams.formMode === "edit") {
       this.reqPokemon.getPokemon(this.route.snapshot.params.number).subscribe(pokemon => {
         this.pokemon = pokemon;
         console.log(this.pokemon);
       });
       this.isOnEditMode = true;
-    }else{
+    } else {
       this.pokemon = {};
     }
+  }
+
+  registerPokemon(pokemonForm: NgForm) {
+    console.log(pokemonForm.value);
+  }
+
+  editPokemon(pokemonForm: NgForm) {
+    console.log(pokemonForm.value);
+  }
+
+  addEvolution(pokemonEvolution) {
+    console.log(pokemonEvolution);
+    this.evolutions.push(pokemonEvolution);
+    this.evolutions.sort((a, b) => {
+      if (a.number > b.number) return 1;
+      if (a.number < b.number) return -1;
+      else return 0;
+    });
+  }
+
+  removeEvolution(evolutionArrId) {
+    this.evolutions.splice(evolutionArrId,1);
   }
 
 }
